89a9a2585ee127c58ce830b31813c08d98f7cc2c
ropsetipopseti <92710293+ropsetipopseti@users.noreply.github.com>
2024-03-16 13:51:24 +0200

    changes
    lisätty pelaaja model, muokattu admin oikeuksia

--- board_games/admin.py
+++ board_games/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import BoardGame
+from .models import BoardGame, Player
 
 admin.site.register(BoardGame)
+admin.site.register(Player)
--- board_games/models.py
+++ board_games/models.py
@@ -5,5 +5,18 @@ class BoardGame(models.Model):
     description = models.TextField()
     year = models.IntegerField()
 
+    class Meta:
+        verbose_name_plural = 'boardgames'
+
+    def __str__(self):
+        return self.name
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'players'
+
     def __str__(self):
         return self.name
